92b30bdb284e43fd0164f6b63fdcca5a2642cf4b
Darshana Hettiarachchi <dnhnuwan@gmail.com>
2022-03-02 16:01:01 +0530

    Add second table to data obj and serve as an api.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const readXlsxFile = require("read-excel-file/node");
 
+const express = require("express");
+const app = express();
+const port = 3000;
+
 const readExcel = async () => {
   const rows = await readXlsxFile("./file2.xlsx");
   return rows
@@ -10,32 +14,53 @@ const readExcel = async () => {
 const extractValues = async () => {
   const allRows = await readExcel();
 
-  allRows.filter((intArr, index) => {
-    const hasContractNo = intArr.indexOf("Contract No") > -1;
-
-    if (hasContractNo) {
-      //console.log(allRows[index + 1]);
-
+  const dataMapped = allRows.map((row, index) => {
+    const hasContactNum = row.indexOf("Contract No") > -1;
+    if (hasContactNum) {
+      //console.log(row);
+      const contractNo = allRows[index + 1][2];
       const sizesT1 = allRows[index].slice(7);
-      const quantities = allRows[index + 1].slice(7);
-      //console.log("Sizes2", sizesT2);
-      //console.log("Quantities", quantities);
-      const sizesAndQuantities = sizesT1.map((size, index) => [
+      const quantitiesT1 = allRows[index + 1].slice(7);
+      const sizesT2 = allRows[index + 3].includes("Size")
+        ? allRows[index + 3].slice(1)
+        : [];
+      const quantitiesT2 = allRows[index + 4].includes("Quantity")
+        ? allRows[index + 4].slice(1)
+        : [];
+
+      const sizesAndQuantitiesT1 = sizesT1.map((size, index) => [
         size,
-        quantities[index],
+        quantitiesT1[index],
       ]);
+
+      const sizesAndQuantitiesT2 = sizesT2.map((size, index) => [
+        size,
+        quantitiesT2[index],
+      ]);
+
       const contractObj = {
         contractNo: allRows[index + 1][2],
-        sizesAndQuantities,
+        sizesT1,
+        quantitiesT1,
+        sizesT2,
+        quantitiesT2,
+        sizesAndQuantities: [...sizesAndQuantitiesT1, ...sizesAndQuantitiesT2],
       };
-      //console.log("SizesAndQuantities", sizesAndQuantities);
-      console.log(contractObj);
+
+      return contractObj;
     }
 
-    return hasContractNo;
+    return null;
   });
 
-  //console.log(allRows);
+  return dataMapped.filter((row) => row !== null);
 };
 
-extractValues();
+app.get("/", async (req, res) => {
+  const data = await extractValues();
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
